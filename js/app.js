a110004f7d6671ab815e3a6ce9b0e7052369748f
angelajiaqichen <57929028+angelajiaqichen@users.noreply.github.com>
2023-04-11 15:29:48 -0400

    modified colors and aesthetics

--- js/app.js
+++ js/app.js
@@ -1,34 +1,11 @@
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-const svg = d3.select("#map")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
-
-const projection = d3.geoMercator()
-  .scale(150)
-  .translate([width / 2, height / 1.4]);
-
-const path = d3.geoPath().projection(projection);
-
-d3.json("https://d3js.org/world-110m.v1.json").then(data => {
-  const countries = topojson.feature(data, data.objects.countries).features;
-
-  svg.selectAll(".land")
-    .data(countries)
-    .enter()
-    .append("path")
-    .attr("class", "land")
-    .attr("d", path);
-
-  // Add a marker to the map
-  const marker = svg.append("circle")
-    .attr("class", "marker")
-    .attr("r", 5)
-    .attr("cx", projection([-74.0060, 40.7128])[0])
-    .attr("cy", projection([-74.0060, 40.7128])[1])
-    .on("click", () => {
-      alert("You clicked on the New York City marker!");
-    });
+// make the bottom bar appear only when the user has scrolled to the bottom of the page
+// NOT working!
+window.addEventListener('scroll', function() {
+  var bottomBar = document.getElementById('bottom-bar');
+  var scrollPosition = window.innerHeight + window.pageYOffset;
+  if (scrollPosition >= document.body.offsetHeight) {
+    bottomBar.style.display = 'block';
+  } else {
+    bottomBar.style.display = 'none';
+  }
 });
