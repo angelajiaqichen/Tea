9bfabe2c37a783e5215dddec438668c220f1ce7b
angelajiaqichen <57929028+angelajiaqichen@users.noreply.github.com>
2023-04-11 16:14:39 -0400

    added new icon

--- js/leaflet.js
+++ js/leaflet.js
@@ -21,12 +21,12 @@ var points = [
 ];
 
 var greenIcon = new L.icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
+  iconUrl: 'https://raw.githubusercontent.com/angelajiaqichen/Tea/main/img/roundicon.png',
+  // shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 25],
+  // iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41]
+  // shadowSize: [41, 41]
 });
 
 
