87868db2df741f718f112904c894e5694e9531a5
angelajiaqichen <57929028+angelajiaqichen@users.noreply.github.com>
2023-04-11 14:14:00 -0400

    got map to work. created basic structure of index page

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,9 +1,25 @@
-const map = L.map("map").setView([51.505, -0.09], 3);
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Add a marker to the map
-const marker = L.marker([40.7128, -74.0060]).addTo(map);
-marker.bindPopup("<b>New York City</b>").openPopup();
+// Add a GeoJSON layer to the map
+const geojsonLayer = L.geoJSON(worldGeoJSON).addTo(map);
+
+// Add a click event listener to the GeoJSON layer
+geojsonLayer.on("click", function (e) {
+  const name = e.layer.feature.properties.name;
+  document.getElementById("info").innerHTML = "<h2>" + name + "</h2>";
+});
+
+// Load the world GeoJSON data
+fetch("world-110m.geojson")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // Add the GeoJSON data to the GeoJSON layer
+    geojsonLayer.addData(data);
+  });
