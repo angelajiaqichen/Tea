716f1e95c2d875a0fcb97af3cb9b5f5e8fad4f81
angelajiaqichen <57929028+angelajiaqichen@users.noreply.github.com>
2023-04-30 23:26:04 -0400

    display incorrect questions in game

--- js/game.js
+++ js/game.js
@@ -29,154 +29,152 @@ const allQuestions = [
       answer: "Ceylon",
       image: "img/ceylon.png"
     },
+  {
+      question: "What is the name of the traditional Japanese tea ceremony?",
+      options: ["Chado", "Gongfu", "Yumcha", "Teaism"],
+      answer: "Chado",
+      image: "img/matcha2.png"
+    },
     {
-        question: "What is the name of the traditional Japanese tea ceremony?",
-        options: ["Chado", "Gongfu", "Yumcha", "Teaism"],
-        answer: "Chado",
-        image: "img/matcha2.png"
-      },
-      {
-        question: "What is the name of the famous tea-growing region in India?",
-        options: ["Darjeeling", "Assam", "Nilgiri", "Ceylon"],
-        answer: "Darjeeling",
-        image: "img/darjeeling.png"
-      },
-      {
-        question: "What is the name of the Chinese tea that is dried over smoking pine fires?",
-        options: ["Dragonwell", "Lapsang Souchong", "Tie Guan Yin", "Keemun"],
-        answer: "Lapsang Souchong",
-        image: "img/fire.png"
-      },
-      {
-        question: "What is the name of the type of tea that is fermented and aged, and is popular in China and Tibet?",
-        options: ["Kombucha", "Pu-erh", "Butter Tea", "Yak Milk Tea"],
-        answer: "Pu-erh",
-        image: "img/puer.png"
-      },
-      {
-        question: "What is the name of the famous tea that is grown in the Nilgiri Hills of India?",
-        options: ["Darjeeling", "Assam", "Nilgiri", "Ceylon"],
-        answer: "Nilgiri",
-        image: "img/nilgiri.png"
-      },
-      {
-        question: "What is the name of the tea that is traditionally served with dim sum in Chinese cuisine?",
-        options: ["Jasmine Tea", "Oolong Tea", "Green Tea", "Pu-erh Tea"],
-        answer: "Pu-erh Tea",
-        image: "img/puer.png"
-      },
-      {
-        question: "What is the name of the tea that is blended with bergamot oil to make Earl Grey tea?",
-        options: ["Darjeeling", "Assam", "Nilgiri", "Ceylon"],
-        answer: "Darjeeling",
-        image: "img/darjeeling.png"
-      },
-      {
-        question: "What is the name of the type of Japanese tea that is powdered and whisked into a frothy emulsion?",
-        options: ["Sencha", "Gyokuro", "Matcha", "Hojicha"],
-        answer: "Matcha",
-        image: "img/matcha.png"
-      },
-      {
-        question: "What is the name of the type of tea that is commonly used to make Moroccan mint tea?",
-        options: ["Green Tea", "Black Tea", "Oolong Tea", "Gunpowder Tea"],
-        answer: "Gunpowder Tea",
-        image: "img/gunpowder.png"
-      },
-      {
-        question: "What is the name of the type of tea that is made from the leaves of the South African rooibos plant?",
-        options: ["Black Tea", "Green Tea", "Oolong Tea", "Rooibos Tea"],
-        answer: "Rooibos Tea",
-        image: "img/rooibos.png"
-    }
-  ];
+      question: "What is the name of the famous tea-growing region in India?",
+      options: ["Darjeeling", "Assam", "Nilgiri", "Ceylon"],
+      answer: "Darjeeling",
+      image: "img/darjeeling.png"
+    },
+    {
+      question: "What is the name of the Chinese tea that is dried over smoking pine fires?",
+      options: ["Dragonwell", "Lapsang Souchong", "Tie Guan Yin", "Keemun"],
+      answer: "Lapsang Souchong",
+      image: "img/fire.png"
+    },
+    {
+      question: "What is the name of the type of tea that is fermented and aged, and is popular in China and Tibet?",
+      options: ["Kombucha", "Pu-erh", "Butter Tea", "Yak Milk Tea"],
+      answer: "Pu-erh",
+      image: "img/puer.png"
+    },
+    {
+      question: "What is the name of the famous tea that is grown in the Nilgiri Hills of India?",
+      options: ["Darjeeling", "Assam", "Nilgiri", "Ceylon"],
+      answer: "Nilgiri",
+      image: "img/nilgiri.png"
+    },
+    {
+      question: "What is the name of the tea that is traditionally served with dim sum in Chinese cuisine?",
+      options: ["Jasmine Tea", "Oolong Tea", "Green Tea", "Pu-erh Tea"],
+      answer: "Pu-erh Tea",
+      image: "img/puer.png"
+    },
+    {
+      question: "What is the name of the tea that is blended with bergamot oil to make Earl Grey tea?",
+      options: ["Darjeeling", "Assam", "Nilgiri", "Ceylon"],
+      answer: "Darjeeling",
+      image: "img/darjeeling.png"
+    },
+    {
+      question: "What is the name of the type of Japanese tea that is powdered and whisked into a frothy emulsion?",
+      options: ["Sencha", "Gyokuro", "Matcha", "Hojicha"],
+      answer: "Matcha",
+      image: "img/matcha.png"
+    },
+    {
+      question: "What is the name of the type of tea that is commonly used to make Moroccan mint tea?",
+      options: ["Green Tea", "Black Tea", "Oolong Tea", "Gunpowder Tea"],
+      answer: "Gunpowder Tea",
+      image: "img/gunpowder.png"
+    },
+    {
+      question: "What is the name of the type of tea that is made from the leaves of the South African rooibos plant?",
+      options: ["Black Tea", "Green Tea", "Oolong Tea", "Rooibos Tea"],
+      answer: "Rooibos Tea",
+      image: "img/rooibos.png"
+  }
+];
 
-  function getRandomQuestions(numQuestions) {
-    // Shuffle the questions array using "Fisher-Yates shuffle algorithm"
-    for (let i = allQuestions.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [allQuestions[i], allQuestions[j]] = [allQuestions[j], allQuestions[i]];
-    }
-  
-    // Return the first numQuestions questions in the shuffled array
-    return allQuestions.slice(0, numQuestions);
+function getRandomQuestions(numQuestions) {
+  // Shuffle the questions array using "Fisher-Yates shuffle algorithm"
+  for (let i = allQuestions.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [allQuestions[i], allQuestions[j]] = [allQuestions[j], allQuestions[i]];
   }
-  
-  const questions = getRandomQuestions(5);
-  let currentQuestion = 0;
-  let score = 0;
-  
+
+  // Return the first numQuestions questions in the shuffled array
+  return allQuestions.slice(0, numQuestions);
+}
+
+const questions = getRandomQuestions(5);
+let currentQuestion = 0;
+let score = 0;
+let incorrectAnswers = []; // initialize an array to store the incorrect answers
+
 //   const questionElement = document.getElementById("question");
-  const questionElement = document.querySelector("#question");
-  const optionsContainer = document.querySelector("#options-container");
-  const imageElement = document.querySelector("#question-image");
-//   console.log(optionsContainer.options)
-//   console.log(document.querySelector(".option"))
-//   console.log(document.querySelector("#reset"))
-  const scoreElement = document.getElementById("score");
-//   var selected = "";
-//   console.log(op1.innerText)
-//   op1.addEventListener("click", () => {
-//     selected = op1.innerText;
-//     console.log(selected);
-//   })
-  
-  function showQuestion() {
-    // console.log(currentQuestion)
-    // console.log(questions[currentQuestion].question)
-    questionElement.innerText = questions[currentQuestion].question; // set the question text
-    imageElement.src = questions[currentQuestion].image;
-    optionsContainer.innerHTML = ""; // clear the options container
-    for (let i = 0; i < questions[currentQuestion].options.length; i++) {
-      const optionButton = document.createElement("button");
-      optionButton.classList.add("option");
-      optionButton.innerText = questions[currentQuestion].options[i];
-      optionsContainer.appendChild(optionButton);
-      optionButton.addEventListener("click", () => checkAnswer(optionButton));
-    }
+const questionElement = document.querySelector("#question");
+const optionsContainer = document.querySelector("#options-container");
+const imageElement = document.querySelector("#question-image");
+const scoreElement = document.getElementById("score");
+
+
+function showQuestion() {
+  // console.log(currentQuestion)
+  // console.log(questions[currentQuestion].question)
+  questionElement.innerText = questions[currentQuestion].question; // set the question text
+  imageElement.src = questions[currentQuestion].image;
+  optionsContainer.innerHTML = ""; // clear the options container
+  for (let i = 0; i < questions[currentQuestion].options.length; i++) {
+    const optionButton = document.createElement("button");
+    optionButton.classList.add("option");
+    optionButton.innerText = questions[currentQuestion].options[i];
+    optionsContainer.appendChild(optionButton);
+    optionButton.addEventListener("click", () => checkAnswer(optionButton));
   }
-  
-  function checkAnswer(selectedButton) {
-    console.log(questions[currentQuestion].answer)
-    console.log(selectedButton.innerText)
-    if (selectedButton.innerText == questions[currentQuestion].answer) {
-      score++;
-    }
-    // console.log(score)
-    scoreElement.innerText = "Score: " + score;
-    currentQuestion++;
-    if (currentQuestion >= questions.length) {
-      endGame();
-    } else {
-      showQuestion();
-    }
+}
+
+function checkAnswer(selectedButton) {
+  console.log(questions[currentQuestion].answer)
+  console.log(selectedButton.innerText)
+  if (selectedButton.innerText == questions[currentQuestion].answer) {
+    score++;
+  } else {
+    // add the incorrect question and its correct answer to the array
+    incorrectAnswers.push({
+      question: questions[currentQuestion].question,
+      answer: questions[currentQuestion].answer
+    });
   }
-  
-  function endGame() {
-    questionElement.innerText = "Game Over! You scored " + score + " out of " + questions.length + " points." + " \n Do you want to play again?";
-    optionsContainer.innerHTML = "";
-    scoreElement.innerText = "";
-    imageElement.style.display = "none"; // hide the image container
+  scoreElement.innerText = "Score: " + score;
+  currentQuestion++;
+  if (currentQuestion >= questions.length) {
+    endGame();
+  } else {
+    showQuestion();
   }
+}
+
+function endGame() {
+  questionElement.innerText = "Game Over! You scored " + score + " out of " + questions.length + " points." + " \n ";
+  optionsContainer.innerHTML = "";
+  scoreElement.innerText = "Do you want to play again?";
+  imageElement.style.display = "none"; // hide the image container
+  // display the list of incorrectly answered questions
+  if (incorrectAnswers.length > 0) {
+    questionElement.innerText = "Game Over! You scored " + score + " out of " + questions.length + " points." + " \n Here are the ones you missed!";
+    const incorrectList = document.createElement("ul");
+    for (let i = 0; i < incorrectAnswers.length; i++) {
+      const incorrectItem = document.createElement("li");
+      incorrectItem.innerText = "Q: " + incorrectAnswers[i].question + "\nA: " + incorrectAnswers[i].answer + "\n\n\n";
+      incorrectList.appendChild(incorrectItem);
+    }
+    optionsContainer.appendChild(incorrectList);
+  }
+}
   
-//   function resetGame() {
-//     console.log("restarted the game")
-//     const questions = getRandomQuestions(5);
-//     // console.log(questions)
-//     currentQuestion = 0;
-//     score = 0;
-//     showQuestion();
-//     // console.log(questions[currentQuestion].question)
-//     // console.log(questionElement)
-//     scoreElement.innerText = "Score: " + score;
-//   }
 
 function startGame() {
     window.location.href = "game.html";
 }
 
-  function resetGame() {
-    window.location.reload();
-  }
-  showQuestion();
-  scoreElement.innerText = "Score: " + score;
+function resetGame() {
+  window.location.reload();
+}
+showQuestion();
+scoreElement.innerText = "Score: " + score;
